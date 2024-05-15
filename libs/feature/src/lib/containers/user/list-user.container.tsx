2167e7ae6dd4383980f2b6371e8cef1693d2d4df
ErickEmpresa <erickcelestimo@gmail.com>
2024-05-15 11:51:57 -0300

    feat: add notify card from page

--- libs/feature/src/lib/containers/user/list-user.container.tsx
+++ libs/feature/src/lib/containers/user/list-user.container.tsx
@@ -3,12 +3,16 @@ import { ListUser, SearchUser } from '../../components';
 import { LayoutBase } from '../../layout';
 import { useEffect, useState } from 'react';
 import { ListUserRequest } from '../../services';
-import { UserList } from '@workspaces/domain';
+import { ErrorResponse, UserList } from '@workspaces/domain';
+import { useSnackbarAlert } from '../../hooks';
+import axios, { AxiosError } from 'axios';
+import { ConnectionError } from '../../shared';
 
 export const ListUserContainer = () => {
   const [userList, setUserList] = useState<UserList[]>([]);
   const [totalPage, setTotalPage] = useState<number>(1);
   const theme = useTheme();
+  const { showSnackbarAlert, SnackbarAlert } = useSnackbarAlert();
 
   useEffect(() => {
     const getData = async () => {
@@ -18,6 +22,13 @@ export const ListUserContainer = () => {
     };
     getData();
   }, []);
+
+  const showErrorAlert = (message: string) => {
+    showSnackbarAlert({
+      message: message,
+      severity: 'error',
+    });
+  };
 
   const handleChange = async (
     event: React.ChangeEvent<unknown>,
@@ -31,41 +42,60 @@ export const ListUserContainer = () => {
   };
 
   const handleData = async (text: string) => {
-    const result = await ListUserRequest({ input: text });
-    setUserList(result.users);
+    try {
+      const result = await ListUserRequest({ input: text });
+      setUserList(result.users);
+    } catch (error) {
+      console.error((error as { message: string }).message);
+      if (axios.isAxiosError(error)) {
+        const axiosError = error as AxiosError<ErrorResponse>;
+        switch (axiosError.response?.data.error.name) {
+          default:
+            showErrorAlert(ConnectionError('PT-BR'));
+            break;
+        }
+      }
+    }
   };
 
   return (
-    <LayoutBase title="Listagem de Usuários">
-      <Box display="flex" justifyContent="center">
-        <Box width="70%">
-          <SearchUser onSearch={handleData} placeholder="Pesquisar Usuário" />
-          <Box width="95%">
-            {userList.length > 0 ? (
-              userList.map((user) => (
-                <ListUser
-                  key={user.userId}
-                  image="teste"
-                  imageAlt={`Image from ${user.name}`}
-                  name={`Nome: ${user.name}`}
-                  userId={`ID: ${user.userId}`}
-                  email={`Email: ${user.email}`}
-                  nickname={`Nickname: ${user.nickname}`}
-                />
-              ))
-            ) : (
-              <div>Sem Dados</div>
-            )}
-          </Box>
-          <Box marginTop={theme.spacing(2)} display="flex" justifyContent="end">
-            <Pagination
-              count={totalPage}
-              color="primary"
-              onChange={handleChange}
-            />
+    <>
+      <LayoutBase title="Listagem de Usuários">
+        <Box display="flex" justifyContent="center">
+          <Box width="70%">
+            <SearchUser onSearch={handleData} placeholder="Pesquisar Usuário" />
+            <Box width="95%">
+              {userList.length > 0 ? (
+                userList.map((user) => (
+                  <ListUser
+                    key={user.userId}
+                    image="teste"
+                    imageAlt={`Image from ${user.name}`}
+                    name={`Nome: ${user.name}`}
+                    userId={`ID: ${user.userId}`}
+                    email={`Email: ${user.email}`}
+                    nickname={`Nickname: ${user.nickname}`}
+                  />
+                ))
+              ) : (
+                <div>Sem Dados</div>
+              )}
+            </Box>
+            <Box
+              marginTop={theme.spacing(2)}
+              display="flex"
+              justifyContent="end"
+            >
+              <Pagination
+                count={totalPage}
+                color="primary"
+                onChange={handleChange}
+              />
+            </Box>
           </Box>
         </Box>
-      </Box>
-    </LayoutBase>
+      </LayoutBase>
+      {SnackbarAlert}
+    </>
   );
 };
