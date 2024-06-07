662d8c4111a8b03042bb7b8f4643210a714b426d
Erick Celestino <erickcelestimo@gmail.com>
2024-06-06 23:38:51 -0300

    refactor: ajust warning add callback reference

--- libs/feature/src/lib/components/file/files-upload.tsx
+++ libs/feature/src/lib/components/file/files-upload.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useEffect } from 'react';
+import React, { useState, useCallback } from 'react';
 import { useDropzone } from 'react-dropzone';
 import {
   Box,
@@ -18,7 +18,7 @@ import {
 } from '@mui/icons-material';
 import { FileWithProgress } from '@workspaces/domain';
 import { useLoggedUser } from '../../contexts';
-import { CreateContenVideoRequest, getItemLocalStorage } from '../../services';
+import { CreateContenVideoRequest } from '../../services';
 
 interface FilesUploadProps {
   height: string;
@@ -32,8 +32,22 @@ export const FilesUpload: React.FC<FilesUploadProps> = ({
   directoryId,
 }) => {
   const [selectedFiles, setSelectedFiles] = useState<FileWithProgress[]>([]);
-
   const { loggedUser } = useLoggedUser();
+
+  const uploadFiles = useCallback(
+    async (filesWithProgress: FileWithProgress[]) => {
+      try {
+        const loggedUserId = loggedUser?.id ?? '';
+        await CreateContenVideoRequest(filesWithProgress, {
+          directoryId: directoryId,
+          loggedUserId,
+        });
+      } catch (err) {
+        console.error(err);
+      }
+    },
+    [directoryId, loggedUser]
+  );
 
   const onDrop = useCallback(
     (acceptedFiles: File[]) => {
@@ -44,20 +58,8 @@ export const FilesUpload: React.FC<FilesUploadProps> = ({
       setSelectedFiles((prevFiles) => [...prevFiles, ...filesWithProgress]);
       uploadFiles(filesWithProgress);
     },
-    [selectedFiles]
+    [uploadFiles]
   );
-
-  const uploadFiles = async (filesWithProgress: FileWithProgress[]) => {
-    try {
-      const loggedUserId = loggedUser?.id ?? '';
-      await CreateContenVideoRequest(filesWithProgress, {
-        directoryId: directoryId,
-        loggedUserId,
-      });
-    } catch (err) {
-      console.error(err);
-    }
-  };
 
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
@@ -169,3 +171,190 @@ export const FilesUpload: React.FC<FilesUploadProps> = ({
     </Box>
   );
 };
+/*
+
+/*
+import React, { useState, useCallback } from 'react';
+import { useDropzone } from 'react-dropzone';
+import {
+  Box,
+  Button,
+  Typography,
+  IconButton,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemSecondaryAction,
+  Paper,
+  LinearProgress,
+} from '@mui/material';
+import {
+  CloudUpload as CloudUploadIcon,
+  Delete as DeleteIcon,
+} from '@mui/icons-material';
+import { FileWithProgress } from '@workspaces/domain';
+import { useLoggedUser } from '../../contexts';
+import { CreateContenVideoRequest } from '../../services';
+
+interface FilesUploadProps {
+  height: string;
+  width: string;
+  directoryId: string;
+}
+
+export const FilesUpload: React.FC<FilesUploadProps> = ({
+  height,
+  width,
+  directoryId,
+}) => {
+  const [selectedFiles, setSelectedFiles] = useState<FileWithProgress[]>([]);
+  const [filePickerActive, setFilePickerActive] = useState(false);
+  const { loggedUser } = useLoggedUser();
+
+  const uploadFiles = useCallback(async (filesWithProgress: FileWithProgress[]) => {
+    try {
+      const loggedUserId = loggedUser?.id ?? '';
+      await CreateContenVideoRequest(filesWithProgress, {
+        directoryId: directoryId,
+        loggedUserId,
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  }, [directoryId, loggedUser]);
+
+  const onDrop = useCallback(
+    (acceptedFiles: File[]) => {
+      const filesWithProgress = acceptedFiles.map((file) => ({
+        file,
+        progress: 0,
+      }));
+      setSelectedFiles((prevFiles) => [...prevFiles, ...filesWithProgress]);
+      uploadFiles(filesWithProgress);
+    },
+    [uploadFiles]
+  );
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+  const handleDeleteFile = (fileName: string) => {
+    const updatedFiles = selectedFiles.filter(
+      (file) => file.file.name !== fileName
+    );
+    setSelectedFiles(updatedFiles);
+  };
+
+  const handleFileInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files) {
+      const filesArray = Array.from(event.target.files);
+      const filesWithProgress = filesArray.map((file) => ({
+        file,
+        progress: 0,
+      }));
+      setSelectedFiles((prevFiles) => [
+        ...prevFiles,
+        ...filesWithProgress,
+      ]);
+      uploadFiles(filesWithProgress);
+    }
+  };
+
+  const handleFileButtonClick = () => {
+    if (!filePickerActive) {
+      setFilePickerActive(true);
+      const fileInput = document.createElement('input');
+      fileInput.type = 'file';
+      fileInput.multiple = true;
+      fileInput.onchange = (event: any) => {
+        handleFileInputChange(event);
+        setFilePickerActive(false);
+      };
+      fileInput.click();
+    }
+  };
+
+  return (
+    <Box>
+      <Paper
+        {...getRootProps()}
+        sx={{
+          p: 2,
+          textAlign: 'center',
+          color: 'text.secondary',
+          border: '2px dashed grey',
+          cursor: 'pointer',
+          mb: 2,
+          height: height,
+          width: width,
+        }}
+      >
+        <input {...getInputProps()} />
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100%',
+          }}
+        >
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              maxWidth: '60%',
+            }}
+          >
+            {isDragActive ? (
+              <Typography>Solte os arquivos aqui ...</Typography>
+            ) : (
+              <Typography>
+                Arraste e solte arquivos aqui, ou clique para selecionar
+                arquivos
+              </Typography>
+            )}
+            <Button
+              variant="contained"
+              component="label"
+              startIcon={<CloudUploadIcon />}
+              sx={{
+                mt: 2,
+              }}
+              onClick={handleFileButtonClick}
+            >
+              Upload File
+            </Button>
+          </Box>
+        </Box>
+      </Paper>
+      {selectedFiles.length > 0 && (
+        <Box mt={2}>
+          <Typography marginLeft="1rem" variant="h6">
+            Arquivos Selecionados:
+          </Typography>
+          <List>
+            {selectedFiles.map(({ file, progress }) => (
+              <ListItem key={file.name}>
+                <ListItemText primary={file.name} />
+                <Box width="100%" mx={2}>
+                  <LinearProgress variant="determinate" value={progress} />
+                </Box>
+                <ListItemSecondaryAction>
+                  <IconButton
+                    edge="end"
+                    onClick={() => handleDeleteFile(file.name)}
+                  >
+                    <DeleteIcon />
+                  </IconButton>
+                </ListItemSecondaryAction>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+      )}
+    </Box>
+  );
+};
+
+*/
