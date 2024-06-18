4b8dbce2b87b4a3e995fc6c1db4e7bb69963f8d9
ErickEmpresa <erickcelestimo@gmail.com>
2024-06-18 13:47:43 -0300

    feat: add delete content file and fin content file implementation

---
+++ libs/data-access/src/lib/infra/repository/postgres/file/find-content-file-by-id.repository.ts
@@ -0,0 +1,43 @@
+import { Inject } from '@nestjs/common';
+import { ContentFile, FindContentFileByIdRepository } from '@workspaces/domain';
+import { PrismaService } from 'nestjs-prisma';
+
+export class FindContentFileByIdRepositoryImpl
+  implements FindContentFileByIdRepository
+{
+  constructor(@Inject('PrismaService') private prismaService: PrismaService) {}
+  async find(id: string): Promise<ContentFile> {
+    const contentFileResult = await this.prismaService.content_Files.findFirst({
+      where: {
+        Content_Files_id: id,
+      },
+      select: {
+        Content_Files_id: true,
+        size: true,
+        format: true,
+        upload_date: true,
+        file_name: true,
+        path: true,
+        original_name: true,
+        user: {
+          select: {
+            name: true,
+          },
+        },
+      },
+    });
+
+    const mappedContentFile: ContentFile = {
+      id: contentFileResult?.Content_Files_id ?? '',
+      created_by: contentFileResult?.user.name ?? '',
+      fileName: contentFileResult?.file_name ?? '',
+      format: contentFileResult?.format ?? '',
+      originalName: contentFileResult?.original_name ?? '',
+      path: contentFileResult?.path ?? '',
+      size: contentFileResult?.size ?? '',
+      uploadDate: contentFileResult?.upload_date ?? new Date(),
+    };
+
+    return mappedContentFile;
+  }
+}
