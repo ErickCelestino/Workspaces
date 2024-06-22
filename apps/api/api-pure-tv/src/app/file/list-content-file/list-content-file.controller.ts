88f38ac3af9942f41ddf0b8f6f6728eb9a68378d
Erick Celestino <erickcelestimo@gmail.com>
2024-06-22 18:45:32 -0300

    feat: add list content file route validation

--- apps/api/api-pure-tv/src/app/file/list-content-file/list-content-file.controller.ts
+++ apps/api/api-pure-tv/src/app/file/list-content-file/list-content-file.controller.ts
@@ -1,12 +1,29 @@
-import { BadRequestException, Controller, Get, Query } from '@nestjs/common';
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  Query,
+  UsePipes,
+} from '@nestjs/common';
 import { ListContentFileService } from './list-content-file.service';
-import { ListContentFileDto } from '@workspaces/domain';
+import { ListContentFileDto, listContentFileSchema } from '@workspaces/domain';
+import { ZodValidationPipe } from '../../pipes/zod-validation-pipe';
 
 @Controller('list-content-file')
 export class ListContentFileController {
   constructor(
     private readonly listContentFileService: ListContentFileService
   ) {}
+
+  @UsePipes(
+    new ZodValidationPipe({
+      loggedUserId: listContentFileSchema.loggedUserId,
+      directoryId: listContentFileSchema.directoryId,
+      filter: listContentFileSchema.filter,
+      take: listContentFileSchema.take,
+      skip: listContentFileSchema.skip,
+    })
+  )
   @Get()
   async list(
     @Query('filter') filter: string,
--- libs/domain/src/lib/validations/file/index.ts
+++ libs/domain/src/lib/validations/file/index.ts
@@ -2,3 +2,4 @@ export * from './create-content-file.schema';
 export * from './delete-content-file-by-id.schema';
 export * from './details-content-file.schema';
 export * from './edit-file.schema';
+export * from './list-content-file.schema';
