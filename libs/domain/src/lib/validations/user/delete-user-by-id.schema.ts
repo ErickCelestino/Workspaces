0f4a0caff8e1bdaab520e458071a3daf504cad51
Erick Celestino <erickcelestimo@gmail.com>
2024-06-23 20:41:47 -0300

    feat: add delete user by id validation

--- libs/domain/src/lib/validations/user/delete-user-by-id.schema.ts
+++ libs/domain/src/lib/validations/user/delete-user-by-id.schema.ts
@@ -2,4 +2,8 @@ import { z } from 'zod';
 
 export const deleteUserByIdSchema = {
   id: z.string().min(1),
+  input: z.object({
+    description: z.string().min(1),
+  }),
+  loggedUserId: z.string().min(1),
 };
