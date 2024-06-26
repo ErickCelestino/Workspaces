9edc4490212d30e8a1c97480d0493941bc696555
ErickEmpresa <erickcelestimo@gmail.com>
2024-06-26 14:38:33 -0300

    feat: add move file to directory use case

--- libs/domain/src/lib/use-case/file/index.ts
+++ libs/domain/src/lib/use-case/file/index.ts
@@ -4,3 +4,4 @@ export * from './delete-content-file-by-id';
 export * from './download-content-file';
 export * from './edit-content-file';
 export * from './list-content-file';
+export * from './move-file-to-directory';
