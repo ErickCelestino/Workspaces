a409850200b34a927264db88074c9303216fb3ea
ErickEmpresa <erickcelestimo@gmail.com>
2024-07-01 15:23:31 -0300

    feat: add find playlist category by id implementation

--- libs/data-access/src/lib/infra/repository/postgres/playlist/category/index.ts
+++ libs/data-access/src/lib/infra/repository/postgres/playlist/category/index.ts
@@ -1,3 +1,4 @@
 export * from './create-playlist-category.repository';
+export * from './find-playlist-category-by-id.repository';
 export * from './find-playlist-category-by-name.repository';
 export * from './list-playlist-category.repository';
