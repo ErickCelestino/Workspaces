121e237391ac7f3ffc3c2d3183b3f4a27db70400
ErickEmpresa <erickcelestimo@gmail.com>
2024-07-01 15:14:14 -0300

    test: ensure FindPlaylistCategory use case

--- libs/domain/tests/repository/playlist/category/index.ts
+++ libs/domain/tests/repository/playlist/category/index.ts
@@ -1,3 +1,4 @@
 export * from './create-playlist-category.mock';
+export * from './find-playlist-category-by-id.mock';
 export * from './find-playlist-category-by-name.mock';
 export * from './list-playlist-category.mock';
