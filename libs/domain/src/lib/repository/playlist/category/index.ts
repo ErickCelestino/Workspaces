8dab4c894fe73dd86e9f6ca1e61b48183ee85cbd
Erick da Silva Celestino <99321670+ErickCelestino@users.noreply.github.com>
2024-07-01 15:48:08 -0300

    Find playlist category by (#32)
    * feat: add find playlist category by id use case
    
    * test: ensure FindPlaylistCategory use case
    
    * feat: add find playlist category by id implementation
    
    * feat: add find playlist category by id route

--- libs/domain/src/lib/repository/playlist/category/index.ts
+++ libs/domain/src/lib/repository/playlist/category/index.ts
@@ -1,3 +1,4 @@
 export * from './create-playlist-category';
+export * from './find-playlist-category-by-id';
 export * from './find-playlist-category-by-name';
 export * from './list-playlist-category';
--- libs/domain/src/lib/use-case/playlist/category/index.ts
+++ libs/domain/src/lib/use-case/playlist/category/index.ts
@@ -1,2 +1,3 @@
 export * from './create-playlist-category';
+export * from './find-playlist-category-by-id';
 export * from './list-playlist-category';
