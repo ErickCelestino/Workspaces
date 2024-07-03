a900bbc416720f9a94cf016a6a3c80ac0e2929a5
Erick Celestino <erickcelestimo@gmail.com>
2024-07-02 21:26:43 -0300

    feat: add create playlist and find playlist by name implementation

---
+++ libs/data-access/src/lib/infra/repository/postgres/playlist/find-playlist-by-name.repository.ts
@@ -0,0 +1,47 @@
+import { Inject } from '@nestjs/common';
+import {
+  FindPlaylistByNameDto,
+  FindPlaylistByNameRepository,
+  Playlist,
+} from '@workspaces/domain';
+import { PrismaService } from 'nestjs-prisma';
+
+export class FindPlaylistByIdRepositoryImpl
+  implements FindPlaylistByNameRepository
+{
+  constructor(@Inject('PrismaService') private prismaService: PrismaService) {}
+  async find(input: FindPlaylistByNameDto): Promise<Playlist> {
+    const { loggedUserId, name } = input;
+    const playlistResult = await this.prismaService.playlist.findFirst({
+      where: {
+        name,
+        user_id: loggedUserId,
+      },
+      select: {
+        playlist_id: true,
+        category: {
+          select: {
+            name: true,
+          },
+        },
+        created_at: true,
+        name: true,
+        user: {
+          select: {
+            name: true,
+          },
+        },
+      },
+    });
+
+    const mappedPlaylist: Playlist = {
+      category: playlistResult?.category.name ?? '',
+      created_at: playlistResult?.created_at ?? new Date(),
+      created_by: playlistResult?.user.name ?? '',
+      id: playlistResult?.playlist_id ?? '',
+      name: playlistResult?.name ?? '',
+    };
+
+    return mappedPlaylist;
+  }
+}
