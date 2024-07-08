6e42e4d9ccc85bffd75bdb754014fd0056bd8a7e
ErickEmpresa <erickcelestimo@gmail.com>
2024-07-08 15:32:00 -0300

    feat: add add file to playlist use case

---
+++ libs/domain/src/lib/dto/request/file/file-to-playlist/add-file-to-playlist.dto.ts
@@ -0,0 +1,5 @@
+export interface AddFileToPlaylistDto {
+  loggedUserId: string;
+  fileId: string;
+  playlistId: string;
+}
---
+++ libs/domain/src/lib/use-case/file/file-to-playlist/add-file-to-playlist.ts
@@ -0,0 +1,62 @@
+import { Inject } from '@nestjs/common';
+import { UseCase } from '../../../base/use-case';
+import { AddFileToPlaylistDto } from '../../../dto';
+import { EntityNotCreated, EntityNotEmpty } from '../../../error';
+import { Either, left, right } from '../../../shared/either';
+import {
+  AddFileToPlaylistRepository,
+  FindContentFileByIdRepository,
+  FindPlaylistByIdRepository,
+  FindUserByIdRepository,
+} from '../../../repository';
+import {
+  ValidationContentFileId,
+  ValidationPlaylistId,
+  ValidationUserId,
+} from '../../../utils';
+
+export class AddFileToPlaylist
+  implements UseCase<AddFileToPlaylistDto, Either<EntityNotEmpty, string>>
+{
+  constructor(
+    @Inject('FindUserByIdRepository')
+    private findUserByIdRepository: FindUserByIdRepository,
+    @Inject('FindContentFileByIdRepository')
+    private findContentFileByIdRepository: FindContentFileByIdRepository,
+    @Inject('FindPlaylistByIdRepository')
+    private findPlaylistByIdRepository: FindPlaylistByIdRepository,
+    @Inject('AddFileToPlaylistRepository')
+    private addFileToPlaylistRepository: AddFileToPlaylistRepository
+  ) {}
+  async execute(
+    input: AddFileToPlaylistDto
+  ): Promise<Either<EntityNotEmpty, string>> {
+    const { loggedUserId, fileId, playlistId } = input;
+
+    if (Object.keys(loggedUserId).length < 1) {
+      return left(new EntityNotEmpty('User ID'));
+    }
+
+    if (Object.keys(fileId).length < 1) {
+      return left(new EntityNotEmpty('File ID'));
+    }
+
+    if (Object.keys(playlistId).length < 1) {
+      return left(new EntityNotEmpty('Playlist ID'));
+    }
+
+    await ValidationUserId(loggedUserId, this.findUserByIdRepository);
+
+    await ValidationContentFileId(fileId, this.findContentFileByIdRepository);
+
+    await ValidationPlaylistId(playlistId, this.findPlaylistByIdRepository);
+
+    const filteredResult = await this.addFileToPlaylistRepository.add(input);
+
+    if (Object.keys(filteredResult).length < 1) {
+      return left(new EntityNotCreated('File to Playlist'));
+    }
+
+    return right(filteredResult);
+  }
+}
