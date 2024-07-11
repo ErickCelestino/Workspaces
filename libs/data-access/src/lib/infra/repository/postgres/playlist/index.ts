7fe2e0474d472153757a6f89527e5ff12cc497a1
ErickEmpresa <erickcelestimo@gmail.com>
2024-07-11 10:21:17 -0300

    feat: add details playlist implementation

--- libs/data-access/src/lib/infra/repository/postgres/playlist/index.ts
+++ libs/data-access/src/lib/infra/repository/postgres/playlist/index.ts
@@ -1,6 +1,7 @@
 export * from './category';
 export * from './create-playlist.repository';
 export * from './delete-playlist.repository';
+export * from './details-playlist.repository';
 export * from './edit-playlist';
 export * from './find-playlist-by-id.repository';
 export * from './find-playlist-by-name.repository';
