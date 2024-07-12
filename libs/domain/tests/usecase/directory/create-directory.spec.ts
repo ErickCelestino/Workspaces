6c23ae771b15401196ffb5b94d0cd496feaf63a3
Pura Fome <samuelpe06062000@gmail.com>
2024-07-12 13:00:35 -0300

    feat: create directory full tested

--- libs/domain/tests/usecase/directory/create-directory.spec.ts
+++ libs/domain/tests/usecase/directory/create-directory.spec.ts
@@ -1,15 +1,15 @@
 import {
   CreateDirectoryDto,
   CreateDirectoryRepository,
+  EntityNotEmpty,
   FindDirectoryByNameRepository,
   FindUserByIdRepository,
+  UserList,
 } from '../../../src';
 import { CreateDirectory } from '../../../src/lib/use-case';
 import { DirectoryMock, userMock } from '../../entity';
-import {
-  CreateDirectoryRepositoryMock,
-  FindUserByIdRepositoryMock,
-} from '../../repository';
+import { FindUserByIdRepositoryMock } from '../../repository';
+import { CreateDirectoryRepositoryMock } from '../../repository/directory/create-directory.mock';
 import { FindDirectoryByNameRepositoryMock } from '../../repository/directory/find-directory-by-name.mock';
 
 interface SutTypes {
@@ -50,5 +50,47 @@ describe('CreateDirectory', () => {
     const { sut, CreateDirectoryDto } = makeSut();
     const result = await sut.execute(CreateDirectoryDto);
     expect(result.isRight()).toBe(true);
+  });
+
+  it('garantir que vai retornar erro EntityNotEmpty se o campo name estiver vazio', async () => {
+    const { sut, CreateDirectoryDto } = makeSut();
+    CreateDirectoryDto.name = '';
+    const result = await sut.execute(CreateDirectoryDto);
+    expect(result.isLeft()).toBe(true);
+    expect(result.value).toBeInstanceOf(EntityNotEmpty);
+  });
+
+  it('garantir que vai retornar erro EntityNotEmpty se o campo loggedUserId estiver vazio', async () => {
+    const { sut, CreateDirectoryDto } = makeSut();
+    CreateDirectoryDto.loggedUserId = '';
+    const result = await sut.execute(CreateDirectoryDto);
+    expect(result.isLeft()).toBe(true);
+    expect(result.value).toBeInstanceOf(EntityNotEmpty);
+  });
+
+  it('garantir que vai retornar erro EntityNotExists se o usuário não existir', async () => {
+    const { sut, CreateDirectoryDto, FindUserByIdRepository } = makeSut();
+    jest
+      .spyOn(FindUserByIdRepository, 'find')
+      .mockResolvedValueOnce({} as UserList);
+    const result = await sut.execute(CreateDirectoryDto);
+    expect(result.isLeft()).toBe(true);
+  });
+
+  it('garantir que vai retornar erro EntityAlreadyExists se o diretório já existir', async () => {
+    const { sut, CreateDirectoryDto, FindDirectoryByNameRepository } =
+      makeSut();
+    jest
+      .spyOn(FindDirectoryByNameRepository, 'find')
+      .mockResolvedValueOnce(DirectoryMock.id);
+    const result = await sut.execute(CreateDirectoryDto);
+    expect(result.isLeft()).toBe(true);
+  });
+
+  it('garantir que vai retornar erro EntityNotCreated se o diretório não for criado', async () => {
+    const { sut, CreateDirectoryDto, CreateDirectoryRepository } = makeSut();
+    jest.spyOn(CreateDirectoryRepository, 'create').mockResolvedValueOnce('');
+    const result = await sut.execute(CreateDirectoryDto);
+    expect(result.isLeft()).toBe(true);
   });
 });
