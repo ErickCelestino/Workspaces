2e39b2e25cfaf490a6503eabc3f5a66680e2a7ee
ErickEmpresa <erickcelestimo@gmail.com>
2024-07-18 13:49:59 -0300

    feat: add zod validation in create scheduling route

--- apps/api/api-pure-tv/src/app/create-scheduling/create-scheduling.controller.ts
+++ apps/api/api-pure-tv/src/app/create-scheduling/create-scheduling.controller.ts
@@ -1,10 +1,12 @@
-import { Body, Controller, Post, Query } from '@nestjs/common';
+import { Body, Controller, Post, Query, UsePipes } from '@nestjs/common';
 import { CreateSchedulingService } from './create-scheduling.service';
 import {
   CreateSchedulingBodyDto,
   CreateSchedulingResponseDto,
+  createSchedulingSchema,
   ErrorMessageResult,
 } from '@workspaces/domain';
+import { ZodValidationPipe } from '../pipes/zod-validation-pipe';
 
 @Controller('create-scheduling')
 export class CreateSchedulingController {
@@ -12,6 +14,7 @@ export class CreateSchedulingController {
     private readonly createSchedulingService: CreateSchedulingService
   ) {}
 
+  @UsePipes(new ZodValidationPipe(createSchedulingSchema))
   @Post()
   async create(
     @Query('loggedUserId') loggedUserId: string,
