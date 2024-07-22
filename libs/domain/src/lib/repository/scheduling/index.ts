bcd8a95f2fb35172701b8b501ad238861ef4af68
ErickEmpresa <erickcelestimo@gmail.com>
2024-07-22 15:56:45 -0300

    feat: add delete scheduling use case

--- libs/domain/src/lib/repository/scheduling/index.ts
+++ libs/domain/src/lib/repository/scheduling/index.ts
@@ -1,3 +1,5 @@
 export * from './create-scheduling';
+export * from './delete-scheduling';
+export * from './find-scheduling-by-id';
 export * from './find-scheduling-by-name';
 export * from './list-schedules';
