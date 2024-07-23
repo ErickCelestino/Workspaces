8c5e7f3bac88c26d73881375f0be43bf26470476
ErickEmpresa <erickcelestimo@gmail.com>
2024-07-23 17:43:02 -0300

    test: ensure format date in time function

--- libs/domain/src/lib/utils/format-date-in-time.ts
+++ libs/domain/src/lib/utils/format-date-in-time.ts
@@ -2,6 +2,6 @@ export function FormatDateInTime(time: Date) {
   const horas: number = time.getHours();
   const minutos: number = time.getMinutes();
   return `${horas < 10 ? `0${horas}` : horas}:${
-    minutos ? minutos === 0 : '00'
+    minutos === 0 ? '00' : minutos
   }`;
 }
