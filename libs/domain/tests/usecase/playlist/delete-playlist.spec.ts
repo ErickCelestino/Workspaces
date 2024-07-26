6f9db722257bb94090a33bfb89ee06700abc1f8e
ErickEmpresa <erickcelestimo@gmail.com>
2024-07-26 15:53:51 -0300

    refactor: add validation retur in delete playlist use case

--- libs/domain/tests/usecase/playlist/delete-playlist.spec.ts
+++ libs/domain/tests/usecase/playlist/delete-playlist.spec.ts
@@ -4,8 +4,11 @@ import {
   DeletePlaylistDto,
   DeletePlaylistRepoistory,
   EntityNotEmpty,
+  EntityNotExists,
   FindPlaylistByIdRepository,
   FindUserByIdRepository,
+  PlaylistResponseDto,
+  UserList,
 } from '../../../src';
 import { PlaylistMock, userMock } from '../../entity';
 import {
@@ -92,5 +95,29 @@ describe('DeletePlaylist', () => {
     expect(result.isLeft()).toBe(true);
     expect(result.isRight()).toBe(false);
     expect(result.value).toBeInstanceOf(EntityNotEmpty);
+  });
+
+  it('should return EntityNotExists when a pass incorrect Logged User ID', async () => {
+    const { deletePlaylistDto, sut } = makeSut();
+    jest
+      .spyOn(sut['findUserByIdRepository'], 'find')
+      .mockResolvedValueOnce({} as UserList);
+    const result = await sut.execute(deletePlaylistDto);
+
+    expect(result.isLeft()).toBe(true);
+    expect(result.isRight()).toBe(false);
+    expect(result.value).toBeInstanceOf(EntityNotExists);
+  });
+
+  it('should return EntityNotExists when a pass incorrect Playlist ID', async () => {
+    const { deletePlaylistDto, sut } = makeSut();
+    jest
+      .spyOn(sut['findPlaylistByIdRepository'], 'find')
+      .mockResolvedValueOnce({} as PlaylistResponseDto);
+    const result = await sut.execute(deletePlaylistDto);
+
+    expect(result.isLeft()).toBe(true);
+    expect(result.isRight()).toBe(false);
+    expect(result.value).toBeInstanceOf(EntityNotExists);
   });
 });
