9a895b4b3ded9eea6a2c14114e28ff5c403886b5
ErickEmpresa <erickcelestimo@gmail.com>
2024-07-26 11:35:56 -0300

    refactor: add delete playlist in associative tables

--- libs/data-access/src/lib/infra/repository/postgres/playlist/delete-playlist.repository.ts
+++ libs/data-access/src/lib/infra/repository/postgres/playlist/delete-playlist.repository.ts
@@ -5,6 +5,18 @@ import { PrismaService } from 'nestjs-prisma';
 export class DeletePlaylistRepositoryImpl implements DeletePlaylistRepoistory {
   constructor(@Inject('PrismaService') private prismaService: PrismaService) {}
   async delete(id: string): Promise<void> {
+    await this.prismaService.playlist_X_Scheduling.deleteMany({
+      where: {
+        playlist_id: id,
+      },
+    });
+
+    await this.prismaService.playlist_X_Content_Files.deleteMany({
+      where: {
+        playlist_id: id,
+      },
+    });
+
     await this.prismaService.playlist.delete({
       where: {
         playlist_id: id,
