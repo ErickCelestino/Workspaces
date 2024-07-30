0e033eafdc527d8067cc7375777658e319367847
ErickEmpresa <erickcelestimo@gmail.com>
2024-07-30 11:27:16 -0300

    feat: add create device use case

---
+++ libs/domain/src/lib/dto/request/device/create-device.dto.ts
@@ -0,0 +1,4 @@
+export interface CreateDeviceDto {
+  name: string;
+  loggedUserId: string;
+}
---
+++ libs/domain/src/lib/use-case/device/create-device.ts
@@ -0,0 +1,73 @@
+import { Inject } from '@nestjs/common';
+import { UseCase } from '../../base/use-case';
+import { CreateDeviceDto } from '../../dto';
+import {
+  EntityAlreadyExists,
+  EntityNotCreated,
+  EntityNotEmpty,
+} from '../../error';
+import { Either, left, right } from '../../shared/either';
+import {
+  CreateDeviceRepository,
+  FindDeviceByNameRepository,
+  FindUserByIdRepository,
+} from '../../repository';
+import { ValidationUserId } from '../../utils';
+
+export class CrateDevice
+  implements
+    UseCase<
+      CreateDeviceDto,
+      Either<EntityNotEmpty | EntityAlreadyExists | EntityNotCreated, string>
+    >
+{
+  constructor(
+    @Inject('FindUserByIdRepository')
+    private findUserByIdRepository: FindUserByIdRepository,
+    @Inject('FindDeviceByNameRepository')
+    private findDeviceByNameRepository: FindDeviceByNameRepository,
+    @Inject('CreateDeviceRepository')
+    private createDeviceRepository: CreateDeviceRepository
+  ) {}
+  async execute(
+    input: CreateDeviceDto
+  ): Promise<
+    Either<EntityNotEmpty | EntityAlreadyExists | EntityNotCreated, string>
+  > {
+    const { loggedUserId, name } = input;
+
+    if (Object.keys(loggedUserId).length < 1) {
+      return left(new EntityNotEmpty('User ID'));
+    }
+
+    if (Object.keys(name).length < 1) {
+      return left(new EntityNotEmpty('Name'));
+    }
+
+    const userValidation = await ValidationUserId(
+      loggedUserId,
+      this.findUserByIdRepository
+    );
+
+    if (userValidation.isLeft()) {
+      return left(userValidation.value);
+    }
+
+    const filteredDevice = await this.findDeviceByNameRepository.find({
+      name,
+      loggedUserId,
+    });
+
+    if (Object.keys(filteredDevice).length > 0) {
+      return left(new EntityAlreadyExists('Device'));
+    }
+
+    const createdDeviceId = await this.createDeviceRepository.create(input);
+
+    if (Object.keys(createdDeviceId).length < 1) {
+      return left(new EntityNotCreated('Device'));
+    }
+
+    return right(createdDeviceId);
+  }
+}
