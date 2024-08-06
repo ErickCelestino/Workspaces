7f4b5aea50e9769ad1ce863c368643f8416f222a
ErickEmpresa <erickcelestimo@gmail.com>
2024-08-06 08:40:05 -0300

    feat: add edit device use case

--- libs/domain/src/lib/dto/request/device/index.ts
+++ libs/domain/src/lib/dto/request/device/index.ts
@@ -1,5 +1,6 @@
 export * from './create-device-body.dto';
 export * from './create-device.dto';
 export * from './delete-device.dto';
+export * from './edit-device.dto';
 export * from './find-device-by-name.dto';
 export * from './list-device.dto';
