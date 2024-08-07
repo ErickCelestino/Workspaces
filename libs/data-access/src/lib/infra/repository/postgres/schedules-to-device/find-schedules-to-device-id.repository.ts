466f6ea4cf5658d32b602452c29ad8eff5a79217
ErickEmpresa <erickcelestimo@gmail.com>
2024-08-07 16:04:55 -0300

    feat: add find schedules by device id implementation

---
+++ libs/data-access/src/lib/infra/repository/postgres/schedules-to-device/find-schedules-to-device-id.repository.ts
@@ -0,0 +1,57 @@
+import { Inject } from '@nestjs/common';
+import {
+  FindSchedulesByDeviceIdDto,
+  FindSchedulesByDeviceIdRepository,
+  FormatDateInTime,
+  Scheduling,
+} from '@workspaces/domain';
+import { PrismaService } from 'nestjs-prisma';
+
+export class FindSchedulesByDeviceIdRepositoryImpl
+  implements FindSchedulesByDeviceIdRepository
+{
+  constructor(@Inject('PrismaService') private prismaService: PrismaService) {}
+  async find(input: FindSchedulesByDeviceIdDto): Promise<Scheduling[]> {
+    const filteredSchedules =
+      await this.prismaService.scheduling_X_Device.findMany({
+        where: {
+          device_id: input.idDevice,
+        },
+        select: {
+          scheduling: {
+            select: {
+              scheduling_id: true,
+              name: true,
+              looping: true,
+              start_time: true,
+              end_time: true,
+              created_at: true,
+              priority: true,
+              user: {
+                select: {
+                  nick_name: true,
+                },
+              },
+            },
+          },
+        },
+      });
+
+    const mappedSchedules: Scheduling[] = filteredSchedules.map((item) => {
+      return {
+        id: item.scheduling?.scheduling_id ?? '',
+        name: item.scheduling?.name ?? '',
+        endTime: FormatDateInTime(new Date(item.scheduling?.end_time ?? '')),
+        startTime: FormatDateInTime(
+          new Date(item.scheduling?.start_time ?? '')
+        ),
+        lopping: item.scheduling?.looping ?? false,
+        priority: `${item.scheduling?.priority}` ?? '',
+        createBy: item.scheduling?.user?.nick_name ?? '',
+        createdAt: item.scheduling?.created_at ?? new Date(),
+      };
+    });
+
+    return mappedSchedules;
+  }
+}
