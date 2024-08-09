9276f0754c928588e5ffc6aecec0bba86c19c31e
Erick Celestino <erickcelestimo@gmail.com>
2024-08-08 22:03:24 -0300

    refactor: ajust list simple directory modal

--- libs/feature/src/lib/components/list/list-simple-directory.tsx
+++ libs/feature/src/lib/components/list/list-simple-directory.tsx
@@ -16,6 +16,7 @@ import {
 import FolderIcon from '@mui/icons-material/Folder';
 import ExpandLess from '@mui/icons-material/ExpandLess';
 import ExpandMore from '@mui/icons-material/ExpandMore';
+import LocalHotelIcon from '@mui/icons-material/LocalHotel';
 import { FC, useCallback, useState } from 'react';
 import {
   ContentFile,
@@ -28,6 +29,7 @@ import { ListContentFilesRequest } from '../../services';
 import axios, { AxiosError } from 'axios';
 import { ValidationsError } from '../../shared';
 import { ScrollBox } from '../scroll';
+import { EmptyListResponse } from './simple';
 
 interface ListSimpleDirectoryProps {
   listDirectories: ListDirectoryNameResponseDto[];
@@ -42,15 +44,11 @@ export const ListSimpleDirectory: FC<ListSimpleDirectoryProps> = ({
   addFileToPlaylist,
   buttonTitle = 'Adicionar à Playlist',
 }) => {
-  const [listFiles, setListFiles] = useState<{ [key: string]: ContentFile[] }>(
+  const [listFiles, setListFiles] = useState<Record<string, ContentFile[]>>({});
+  const [openSubItems, setOpenSubItems] = useState<Record<string, boolean>>({});
+  const [selectedFiles, setSelectedFiles] = useState<Record<string, boolean>>(
     {}
   );
-  const [openSubItems, setOpenSubItems] = useState<{ [key: string]: boolean }>(
-    {}
-  );
-  const [selectedFiles, setSelectedFiles] = useState<{
-    [key: string]: boolean;
-  }>({});
   const { loggedUser } = useLoggedUser();
   const theme = useTheme();
   const smDown = useMediaQuery(theme.breakpoints.down('sm'));
@@ -121,103 +119,120 @@ export const ListSimpleDirectory: FC<ListSimpleDirectoryProps> = ({
     <>
       <ScrollBox height={smDown ? theme.spacing(30) : theme.spacing(43)}>
         <List>
-          {listDirectories.map((directory) => (
-            <div key={directory.id}>
-              <ListItem>
-                {openSubItems[directory.id] && (
-                  <ListItemIcon>
-                    <Checkbox
-                      edge="start"
-                      checked={
-                        listFiles[directory.id]?.length > 0 &&
-                        listFiles[directory.id].every(
-                          (file) => selectedFiles[file.id]
-                        )
-                      }
-                      onChange={() => handleDirectoryToggle(directory.id)}
-                    />
-                  </ListItemIcon>
-                )}
-                <ListItemButton
-                  sx={{
-                    marginLeft: openSubItems[directory.id]
-                      ? theme.spacing(-5)
-                      : 0,
-                  }}
-                  key={directory.id}
-                  onClick={() => handleToggleSubItems(directory.id)}
-                >
-                  <ListItemAvatar>
-                    <Avatar>
-                      <FolderIcon />
-                    </Avatar>
-                  </ListItemAvatar>
-                  <ListItemText primary={directory.name} />
-                  {openSubItems[directory.id] ? <ExpandLess /> : <ExpandMore />}
-                </ListItemButton>
-              </ListItem>
-              <Collapse
-                in={openSubItems[directory.id]}
-                timeout="auto"
-                unmountOnExit
-              >
-                <List
-                  component="div"
-                  disablePadding
-                  sx={{
-                    marginLeft: smDown ? theme.spacing(1) : theme.spacing(3),
-                  }}
-                >
-                  {listFiles[directory.id]?.map((file) => (
-                    <ListItem key={file.id}>
-                      <ListItemIcon
-                        sx={{
-                          marginRight: smDown
-                            ? theme.spacing(-2)
-                            : theme.spacing(0),
-                        }}
-                      >
-                        <Checkbox
-                          edge="start"
-                          checked={!!selectedFiles[file.id]}
-                          onChange={() => handleFileToggle(file.id)}
-                        />
-                      </ListItemIcon>
-                      <ListItemAvatar
-                        sx={{
-                          marginRight: smDown
-                            ? theme.spacing(1)
-                            : theme.spacing(3),
-                        }}
-                      >
-                        <Avatar
-                          sx={{
-                            width: theme.spacing(8),
-                            height: theme.spacing(8),
-                            '& img': {
-                              objectFit: 'contain',
-                              objectPosition: 'center',
-                              maxHeight: '100%',
-                              maxWidth: '100%',
-                            },
-                          }}
-                          src={file.path}
-                        />
-                      </ListItemAvatar>
-                      <ListItemText
-                        sx={{
-                          overflow: 'hidden',
-                          fontSize: smDown ? '8px' : '16px',
-                          textOverflow: 'ellipsis',
-                        }}
-                        primary={file.originalName}
+          {listDirectories.length > 0 ? (
+            listDirectories.map((directory) => (
+              <Box key={directory.id}>
+                <ListItem>
+                  {openSubItems[directory.id] && (
+                    <ListItemIcon>
+                      <Checkbox
+                        edge="start"
+                        checked={
+                          listFiles[directory.id]?.length > 0 &&
+                          listFiles[directory.id].every(
+                            (file) => selectedFiles[file.id]
+                          )
+                        }
+                        onChange={() => handleDirectoryToggle(directory.id)}
                       />
-                    </ListItem>
-                  ))}
-                </List>
-              </Collapse>
-            </div>
-          ))}
+                    </ListItemIcon>
+                  )}
+                  <ListItemButton
+                    sx={{
+                      marginLeft: openSubItems[directory.id]
+                        ? theme.spacing(-5)
+                        : 0,
+                    }}
+                    key={directory.id}
+                    onClick={() => handleToggleSubItems(directory.id)}
+                  >
+                    <ListItemAvatar>
+                      <Avatar>
+                        <FolderIcon />
+                      </Avatar>
+                    </ListItemAvatar>
+                    <ListItemText primary={directory.name} />
+                    {openSubItems[directory.id] ? (
+                      <ExpandLess />
+                    ) : (
+                      <ExpandMore />
+                    )}
+                  </ListItemButton>
+                </ListItem>
+                <Collapse
+                  in={openSubItems[directory.id]}
+                  timeout="auto"
+                  unmountOnExit
+                >
+                  <List
+                    component="div"
+                    disablePadding
+                    sx={{
+                      marginLeft: smDown ? theme.spacing(1) : theme.spacing(3),
+                    }}
+                  >
+                    {listFiles[directory.id]?.map((file) => (
+                      <ListItem key={file.id}>
+                        <ListItemIcon
+                          sx={{
+                            marginRight: smDown
+                              ? theme.spacing(-2)
+                              : theme.spacing(0),
+                          }}
+                        >
+                          <Checkbox
+                            edge="start"
+                            checked={!!selectedFiles[file.id]}
+                            onChange={() => handleFileToggle(file.id)}
+                          />
+                        </ListItemIcon>
+                        <ListItemAvatar
+                          sx={{
+                            marginRight: smDown
+                              ? theme.spacing(1)
+                              : theme.spacing(3),
+                          }}
+                        >
+                          <Avatar
+                            sx={{
+                              width: theme.spacing(8),
+                              height: theme.spacing(8),
+                              '& img': {
+                                objectFit: 'contain',
+                                objectPosition: 'center',
+                                maxHeight: '100%',
+                                maxWidth: '100%',
+                              },
+                            }}
+                            src={file.path}
+                          />
+                        </ListItemAvatar>
+                        <ListItemText
+                          sx={{
+                            overflow: 'hidden',
+                            fontSize: smDown ? '8px' : '16px',
+                            textOverflow: 'ellipsis',
+                          }}
+                          primary={file.originalName}
+                        />
+                      </ListItem>
+                    ))}
+                  </List>
+                </Collapse>
+              </Box>
+            ))
+          ) : (
+            <EmptyListResponse
+              message="Sem Diretórios"
+              icon={
+                <LocalHotelIcon
+                  sx={{
+                    fontSize: theme.spacing(10),
+                  }}
+                />
+              }
+            />
+          )}
         </List>
       </ScrollBox>
       <Box
