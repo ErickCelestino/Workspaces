0392172056972ede3aa916a3eb57275f27c31231
ErickEmpresa <erickcelestimo@gmail.com>
2024-08-12 14:55:01 -0300

    feat: add move schedules to another device use case

--- libs/domain/src/lib/dto/request/schedules-to-device/index.ts
+++ libs/domain/src/lib/dto/request/schedules-to-device/index.ts
@@ -4,3 +4,5 @@ export * from './delete-schedules-to-device.dto';
 export * from './delete-scheduling-to-device.dto';
 export * from './find-schedules-by-device-id.dto';
 export * from './find-scheduling-to-device-by-ids.dto';
+export * from './move-schedules-to-another-device.dto';
+export * from './move-scheduling-to-another-device.dto';
---
+++ libs/domain/src/lib/use-case/schedules-to-device/move-schedules-to-another-device.ts
@@ -0,0 +1,138 @@
+import { Inject } from '@nestjs/common';
+import { UseCase } from '../../base/use-case';
+import { MoveSchedulesToAnotherDeviceDto } from '../../dto';
+import {
+  EntityAlreadyExists,
+  EntityNotEmpty,
+  EntityNotExists,
+  EntityNotMoved,
+} from '../../error';
+import { Either, left, right } from '../../shared/either';
+import {
+  FindDeviceByIdRepository,
+  FindSchedulingByIdRepository,
+  FindSchedulingToDeviceByIdsRepository,
+  FindUserByIdRepository,
+  MoveSchedulingToAnotherDeviceRepository,
+} from '../../repository';
+import {
+  ValidationDeviceId,
+  ValidationSchedulingId,
+  ValidationUserId,
+} from '../../utils';
+
+export class MoveSchedulesToAnotherDevice
+  implements
+    UseCase<MoveSchedulesToAnotherDeviceDto, Either<EntityNotEmpty, string[]>>
+{
+  constructor(
+    @Inject('FindUserByIdRepository')
+    private findUserByIdRepository: FindUserByIdRepository,
+    @Inject('FindDeviceByIdRepository')
+    private findDeviceByIdRepository: FindDeviceByIdRepository,
+    @Inject('FindSchedulingByIdRepository')
+    private findSchedulingByIdRepository: FindSchedulingByIdRepository,
+    @Inject('FindSchedulingToDeviceByIdsRepository')
+    private findSchedulingToDeviceByIdsRepository: FindSchedulingToDeviceByIdsRepository,
+    @Inject('MoveSchedulingToAnotherDeviceRepository')
+    private moveSchedulingToAnotherDeviceRepository: MoveSchedulingToAnotherDeviceRepository
+  ) {}
+  async execute(
+    input: MoveSchedulesToAnotherDeviceDto
+  ): Promise<Either<EntityNotEmpty, string[]>> {
+    const { loggedUserId, newDeviceId, oldDeviceId, schedulesIds } = input;
+    if (Object.keys(loggedUserId).length < 1) {
+      return left(new EntityNotEmpty('loggedUserId'));
+    }
+
+    if (Object.keys(newDeviceId).length < 1) {
+      return left(new EntityNotEmpty('new Device ID'));
+    }
+
+    if (Object.keys(oldDeviceId).length < 1) {
+      return left(new EntityNotEmpty('old Device ID'));
+    }
+
+    if (schedulesIds.length < 1) {
+      return left(new EntityNotEmpty('Schedules'));
+    }
+
+    const userValidation = await ValidationUserId(
+      loggedUserId,
+      this.findUserByIdRepository
+    );
+
+    if (userValidation.isLeft()) {
+      return left(userValidation.value);
+    }
+
+    const oldDeviceValidation = await ValidationDeviceId(
+      oldDeviceId,
+      this.findDeviceByIdRepository
+    );
+
+    if (oldDeviceValidation.isLeft()) {
+      return left(oldDeviceValidation.value);
+    }
+
+    const newDeviceValidation = await ValidationDeviceId(
+      newDeviceId,
+      this.findDeviceByIdRepository
+    );
+
+    if (newDeviceValidation.isLeft()) {
+      return left(newDeviceValidation.value);
+    }
+    const ids = [];
+    for (const scheduleId of schedulesIds) {
+      if (Object.keys(scheduleId).length < 1) {
+        return left(new EntityNotEmpty('Scheduling ID'));
+      }
+
+      const schedulingValidation = await ValidationSchedulingId(
+        scheduleId,
+        this.findSchedulingByIdRepository
+      );
+
+      if (schedulingValidation.isLeft()) {
+        return left(schedulingValidation.value);
+      }
+
+      const filteredSchedulingToOldDevice =
+        await this.findSchedulingToDeviceByIdsRepository.find({
+          idDevice: oldDeviceId,
+          idScheduling: scheduleId,
+        });
+
+      if (Object.keys(filteredSchedulingToOldDevice).length < 1) {
+        return left(new EntityNotExists('Scheduling in old device'));
+      }
+
+      const filteredSchedulingTNewDevice =
+        await this.findSchedulingToDeviceByIdsRepository.find({
+          idDevice: newDeviceId,
+          idScheduling: scheduleId,
+        });
+
+      if (Object.keys(filteredSchedulingTNewDevice).length > 0) {
+        return left(new EntityAlreadyExists('Scheduling in new device'));
+      }
+
+      const movedScheduling =
+        await this.moveSchedulingToAnotherDeviceRepository.move({
+          loggedUserId,
+          newDeviceId,
+          oldDeviceId,
+          schedulingId: scheduleId,
+        });
+
+      if (Object.keys(movedScheduling).length < 1) {
+        return left(new EntityNotMoved('Scheduling'));
+      }
+
+      ids.push(scheduleId);
+    }
+
+    return right(ids);
+  }
+}
