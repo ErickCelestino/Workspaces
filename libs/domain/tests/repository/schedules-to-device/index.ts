e2c71c9f638e5ebe6127addeba3fccbe06d66070
ErickEmpresa <erickcelestimo@gmail.com>
2024-08-12 15:33:57 -0300

    test: ensure schedules to another device use case

--- libs/domain/tests/repository/schedules-to-device/index.ts
+++ libs/domain/tests/repository/schedules-to-device/index.ts
@@ -2,3 +2,4 @@ export * from './add-scheduling-to-device';
 export * from './delete-scheduling-to-device';
 export * from './find-schedules-by-device-id';
 export * from './find-scheduling-to-device-by-ids';
+export * from './move-scheduling-to-another-device';
