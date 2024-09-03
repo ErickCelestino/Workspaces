d5ccc723016c299fe2484f2c10bcbc50a91c2c36
ErickEmpresa <erickcelestimo@gmail.com>
2024-09-03 16:32:16 -0300

    feat: add find company data and edit company data repository implementation

---
+++ libs/data-access/src/lib/infra/repository/postgres/company/data/edit-company-data.repository.ts
@@ -0,0 +1,34 @@
+import { Inject } from '@nestjs/common';
+import {
+  EditCompanyDataDto,
+  EditCompanyDataRepository,
+} from '@workspaces/domain';
+import { PrismaService } from 'nestjs-prisma';
+
+export class EditCompanyDataRepositoryImpl
+  implements EditCompanyDataRepository
+{
+  constructor(@Inject('PrismaService') private prismaService: PrismaService) {}
+  async edit(input: EditCompanyDataDto): Promise<string> {
+    const {
+      body: { legalNature, opening, phone, port, responsibleEmail, situation },
+      companyId,
+    } = input;
+
+    const editedCompany = await this.prismaService.company_Data.update({
+      where: {
+        company_data_id: companyId,
+      },
+      data: {
+        legal_nature: legalNature,
+        opening: opening,
+        phone: phone,
+        port: port,
+        responsible_email: responsibleEmail,
+        situation: situation,
+      },
+    });
+
+    return editedCompany?.company_data_id ?? '';
+  }
+}
