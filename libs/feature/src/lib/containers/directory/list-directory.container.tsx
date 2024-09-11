5630dcbeb6525832ba09699297dec84dd7e404e5
ErickEmpresa <erickcelestimo@gmail.com>
2024-09-11 17:43:08 -0300

    refactor: ajust search data and change data in list directory container ui

--- libs/feature/src/lib/containers/directory/list-directory.container.tsx
+++ libs/feature/src/lib/containers/directory/list-directory.container.tsx
@@ -86,39 +86,31 @@ export const ListDirectoryContainer = () => {
   );
 
   const searchData = async (input: string) => {
-    const result = await handleData({
-      companyId: loggedUser?.selectedCompany.id ?? '',
-      loggedUserId: loggedUser?.id ?? '',
-      userInput: input,
-    });
-
-    setTotalPage(result?.totalPages ?? 0);
-    setListDirectory(result?.directories ?? []);
+    getData(input);
   };
 
   const handleChange = async (
     event: React.ChangeEvent<unknown>,
     value: number
   ) => {
-    const result = await ListDirectoryRequest({
-      userInput: '',
-      companyId: loggedUser?.selectedCompany.id ?? '',
-      loggedUserId: loggedUser?.id ?? '',
-      skip: (value - 1) * 6,
-    });
-    setTotalPage(result.totalPages);
-    setListDirectory(result.directories);
+    getData('', value);
   };
 
-  const getData = useCallback(async () => {
-    const result = await handleData({
-      companyId: loggedUser?.selectedCompany.id ?? '',
-      loggedUserId: loggedUser?.id ?? '',
-      userInput: '',
-    });
-    setTotalPage(result?.totalPages ?? 0);
-    setListDirectory(result?.directories ?? []);
-  }, [handleData, loggedUser]);
+  const getData = useCallback(
+    async (input?: string, skip?: number) => {
+      const result = await handleData({
+        companyId: loggedUser?.selectedCompany.id ?? '',
+        loggedUserId: loggedUser?.id ?? '',
+        userInput: input ? input : '',
+        skip: skip ? (skip - 1) * 6 : 0,
+      });
+      if (result) {
+        setTotalPage(result?.totalPages ?? 0);
+        setListDirectory(result?.directories ?? []);
+      }
+    },
+    [handleData, loggedUser]
+  );
 
   useEffect(() => {
     setIsMounted(false);
