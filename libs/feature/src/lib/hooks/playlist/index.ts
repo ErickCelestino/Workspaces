04db8af8297c6149b5d800975d06f39b24305a29
ErickEmpresa <erickcelestimo@gmail.com>
2024-09-13 14:41:17 -0300

    feat: move list playlist from hook and move playlist category modals from new component ui

--- libs/feature/src/lib/hooks/playlist/index.ts
+++ libs/feature/src/lib/hooks/playlist/index.ts
@@ -1,1 +1,2 @@
+export * from './use-playlist-category-data';
 export * from './use-playlist-data';
---
+++ libs/feature/src/lib/hooks/playlist/use-playlist-category-data.tsx
@@ -0,0 +1,50 @@
+import { useState, useCallback } from 'react';
+import { ListPlaylistCategoryRequest } from '../../services';
+import { ErrorResponse, PlaylistCategory } from '@workspaces/domain';
+import axios, { AxiosError } from 'axios';
+import { ValidationsError } from '../../shared';
+
+interface PlaylistCategoryDataProps {
+  companyId?: string;
+  loggedUserId?: string;
+  showAlert: (message: string, success: boolean) => void;
+}
+
+export const usePlaylistCategoryData = (data: PlaylistCategoryDataProps) => {
+  const { showAlert, loggedUserId, companyId } = data;
+  const [listPlaylistCategory, setListPlaylistCategory] = useState<
+    PlaylistCategory[]
+  >([]);
+  const [totalPage, setTotalPage] = useState<number>(1);
+
+  const getData = useCallback(
+    async (input?: string, skip?: number) => {
+      if (!loggedUserId) return;
+      if (!companyId) return;
+      try {
+        const result = await ListPlaylistCategoryRequest({
+          loggedUserId,
+          companyId,
+          skip: skip ? (skip - 1) * 8 : 0,
+          userInput: input || '',
+        });
+        if (result) {
+          setListPlaylistCategory(result.categories);
+          setTotalPage(result.totalPages);
+        }
+      } catch (error) {
+        console.error(error);
+        if (axios.isAxiosError(error)) {
+          const axiosError = error as AxiosError<ErrorResponse>;
+          const errors = ValidationsError(axiosError, 'Playlist Category');
+          if (errors) {
+            showAlert(errors, false);
+          }
+        }
+      }
+    },
+    [showAlert, loggedUserId]
+  );
+
+  return { listPlaylistCategory, totalPage, getData };
+};
