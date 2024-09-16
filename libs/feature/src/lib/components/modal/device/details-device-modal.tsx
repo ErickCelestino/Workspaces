634ee8eb28e2f3ce5a4ae436287d4be3b0496c51
ErickEmpresa <erickcelestimo@gmail.com>
2024-09-16 09:48:05 -0300

    refactor: add find schedules by device id and find device by id hook and ajust details device modals

--- libs/feature/src/lib/components/modal/device/details-device-modal.tsx
+++ libs/feature/src/lib/components/modal/device/details-device-modal.tsx
@@ -10,21 +10,8 @@ import DeleteSweepIcon from '@mui/icons-material/DeleteSweep';
 import PlaylistRemoveIcon from '@mui/icons-material/PlaylistRemove';
 import OpenWithIcon from '@mui/icons-material/OpenWith';
 import { useLoggedUser } from '../../../contexts';
-import { FC, useCallback, useEffect, useState } from 'react';
-import {
-  Device,
-  ErrorResponse,
-  FindDeviceByIdDto,
-  FindSchedulesByDeviceIdDto,
-  IconMenuItem,
-  Scheduling,
-} from '@workspaces/domain';
-import {
-  FindDeviceByIdRequest,
-  FindSchedulesByDeviceIdRequest,
-} from '../../../services';
-import axios, { AxiosError } from 'axios';
-import { formatBrDate, ValidationsError } from '../../../shared';
+import { FC, useEffect, useState } from 'react';
+import { IconMenuItem } from '@workspaces/domain';
 import { SimpleFormModal } from '../simple';
 import { ButtonFileMenu } from '../../menu';
 import { EmptyListResponse, SchedulingSimpleItem } from '../../list';
@@ -32,6 +19,11 @@ import {
   DeleteSchedulesToDeviceModal,
   MoveSchedulesToAnotherDeviceModal,
 } from '../schedules-to-device';
+import {
+  useFindDeviceByIdData,
+  useFindSchedulesByDeviceIdData,
+} from '../../../hooks';
+import { formatBrDate } from '../../../shared';
 
 interface DetailsDeviceModalProps {
   open: boolean;
@@ -54,58 +46,34 @@ export const DetailsDeviceModal: FC<DetailsDeviceModalProps> = ({
   const theme = useTheme();
   const smDown = useMediaQuery(theme.breakpoints.down('sm'));
 
-  const [schedules, setSchedules] = useState<Scheduling[]>([]);
   const [dataLoaded, setDataLoaded] = useState(false);
-  const [deviceDetails, setDeviceDetails] = useState<Device>({} as Device);
   const [selectedSchedules, setSelectedSchedules] = useState<
     Record<string, boolean>
   >({});
   const [deleteSchedulesPopUp, setDeleteSchedulesPopUp] = useState(false);
   const [moveSchedulesPopUp, setMoveSchedulesPopUp] = useState(false);
-
-  const getDevice = useCallback(
-    async (input: FindDeviceByIdDto) => {
-      try {
-        const result = await FindDeviceByIdRequest(input);
-        setDeviceDetails(result);
-      } catch (error) {
-        console.error(error);
-        if (axios.isAxiosError(error)) {
-          const axiosError = error as AxiosError<ErrorResponse>;
-          const errors = ValidationsError(axiosError, 'Playlist');
-          if (errors) {
-            showAlert(errors, false);
-          }
-        }
-      }
+  const { deviceById, getDeviceByIdData } = useFindDeviceByIdData({
+    input: {
+      id: idDevice,
+      loggedUserId: loggedUser?.id ?? '',
     },
-    [showAlert]
-  );
+    showAlert,
+  });
+
+  const { getListSchedulesByDeviceIdData, listSchedulesByDeviceId } =
+    useFindSchedulesByDeviceIdData({
+      findSchedulesByDeviceIdDto: {
+        idDevice,
+        loggedUserId: loggedUser?.id ?? '',
+      },
+      showAlert,
+    });
 
   const getSelectedSchedulingIds = () => {
     return Object.keys(selectedSchedules).filter(
       (fileId) => selectedSchedules[fileId]
     );
   };
-
-  const getSchedulesToDevice = useCallback(
-    async (input: FindSchedulesByDeviceIdDto) => {
-      try {
-        const result = await FindSchedulesByDeviceIdRequest(input);
-        setSchedules(result);
-      } catch (error) {
-        console.error(error);
-        if (axios.isAxiosError(error)) {
-          const axiosError = error as AxiosError<ErrorResponse>;
-          const errors = ValidationsError(axiosError, 'Arquivos');
-          if (errors) {
-            showAlert(errors, false);
-          }
-        }
-      }
-    },
-    [showAlert]
-  );
 
   useEffect(() => {
     if (!open) {
@@ -115,16 +83,17 @@ export const DetailsDeviceModal: FC<DetailsDeviceModalProps> = ({
 
   useEffect(() => {
     if (open && idDevice && !dataLoaded) {
-      getDevice({
-        loggedUserId: loggedUser?.id ?? '',
-        id: idDevice,
-      });
-      getSchedulesToDevice({
-        idDevice,
-        loggedUserId: loggedUser?.id ?? '',
-      });
+      getDeviceByIdData();
+      getListSchedulesByDeviceIdData();
     }
-  }, [open, idDevice, dataLoaded, getDevice, loggedUser, getSchedulesToDevice]);
+  }, [
+    open,
+    idDevice,
+    dataLoaded,
+    getDeviceByIdData,
+    loggedUser,
+    getListSchedulesByDeviceIdData,
+  ]);
 
   const handleSchedulingToggle = (schedulingId: string) => {
     setSelectedSchedules((prevSelectedSchedules) => {
@@ -208,7 +177,7 @@ export const DetailsDeviceModal: FC<DetailsDeviceModalProps> = ({
             }}
           >
             <strong>Nome: </strong>
-            {deviceDetails?.name ?? ''}
+            {deviceById?.name ?? ''}
           </Typography>
           <Box
             sx={{
@@ -223,11 +192,11 @@ export const DetailsDeviceModal: FC<DetailsDeviceModalProps> = ({
               }}
             >
               <strong>Criado por: </strong>
-              {deviceDetails?.createdBy ?? ''}
+              {deviceById?.createdBy ?? ''}
             </Typography>
             <Typography>
               <strong>Criado em: </strong>
-              {formatBrDate(new Date(deviceDetails?.createdAt ?? new Date()))}
+              {formatBrDate(new Date(deviceById?.createdAt ?? new Date()))}
             </Typography>
           </Box>
           <Divider
@@ -250,8 +219,8 @@ export const DetailsDeviceModal: FC<DetailsDeviceModalProps> = ({
               <ButtonFileMenu iconMenuItemList={iconMenuList} />
             </Box>
             <List>
-              {schedules.length > 0 ? (
-                schedules.map((scheduling) => (
+              {listSchedulesByDeviceId.length > 0 ? (
+                listSchedulesByDeviceId.map((scheduling) => (
                   <SchedulingSimpleItem
                     scheduling={scheduling}
                     key={scheduling.id}
