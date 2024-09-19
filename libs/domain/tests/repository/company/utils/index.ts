a91c6b5c84294dd84faa78fc3ecb01b700a61dba
Erick Celestino <erickcelestimo@gmail.com>
2024-09-19 20:53:51 -0300

    test: ensure select company use case

--- libs/domain/tests/repository/company/utils/index.ts
+++ libs/domain/tests/repository/company/utils/index.ts
@@ -2,3 +2,4 @@ export * from './consult-company-by-cnpj';
 export * from './find-all-company-ids';
 export * from './find-company-by-cnpj';
 export * from './list-users-by-company-id';
+export * from './select-company';
