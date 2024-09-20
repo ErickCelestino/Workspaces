0ed635ce8883837aa8219ea961c079328f4034ba
ErickEmpresa <erickcelestimo@gmail.com>
2024-09-20 14:20:36 -0300

    feat: add authorize user to company implementation

--- libs/data-access/src/lib/infra/repository/postgres/company/utils/index.ts
+++ libs/data-access/src/lib/infra/repository/postgres/company/utils/index.ts
@@ -1,3 +1,4 @@
+export * from './authorize-user-to-company.repository';
 export * from './find-all-company-ids.repository';
 export * from './find-company-by-cnpj.repository';
 export * from './find-user-id-by-company-id.repository';
