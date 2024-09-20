3ec08522c538a021c3f526d8e4ab1fc32093568a
ErickEmpresa <erickcelestimo@gmail.com>
2024-09-20 17:27:30 -0300

    refactor: ajust toolbar pure tv component

--- libs/feature/src/lib/components/toolbar/toolbar-pure-tv.tsx
+++ libs/feature/src/lib/components/toolbar/toolbar-pure-tv.tsx
@@ -1,17 +1,24 @@
 import { IconButton } from '@mui/material';
-import { useAppThemeContext, useFileModal } from '../../contexts';
+import { useFileModal } from '../../contexts';
 import CloudUploadIcon from '@mui/icons-material/CloudUpload';
 
 export const ToolbarPureTV = () => {
   const { handleOpen } = useFileModal();
-  const { themeName } = useAppThemeContext();
 
   return (
-    <IconButton onClick={handleOpen}>
-      <CloudUploadIcon
-        fontSize="large"
-        color={themeName === 'dark' ? 'primary' : 'secondary'}
-      />
+    <IconButton
+      sx={{
+        display: 'flex',
+        background: 'white',
+        justifyContent: 'center',
+        ':hover': {
+          background: 'white',
+          opacity: 0.5,
+        },
+      }}
+      onClick={handleOpen}
+    >
+      <CloudUploadIcon fontSize="large" color="primary" />
     </IconButton>
   );
 };
