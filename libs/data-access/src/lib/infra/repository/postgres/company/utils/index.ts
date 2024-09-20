ad93249298e8a7481b543316f3daf0c8420e0ef9
Erick Celestino <erickcelestimo@gmail.com>
2024-09-19 21:38:24 -0300

    feat: add select company implementation

--- libs/data-access/src/lib/infra/repository/postgres/company/utils/index.ts
+++ libs/data-access/src/lib/infra/repository/postgres/company/utils/index.ts
@@ -1,3 +1,4 @@
 export * from './find-all-company-ids.repository';
 export * from './find-company-by-cnpj.repository';
 export * from './list-users-by-company-id.repository';
+export * from './select-company.repository';
