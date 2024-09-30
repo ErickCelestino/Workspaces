87ee9ce373f9d11c95bb6c468beba21fad56c522
ErickEmpresa <erickcelestimo@gmail.com>
2024-09-30 13:29:37 -0300

    feat: add add user to another company implementation

--- libs/data-access/src/lib/infra/repository/postgres/user/index.ts
+++ libs/data-access/src/lib/infra/repository/postgres/user/index.ts
@@ -1,3 +1,4 @@
+export * from './add-user-to-another-company.repository';
 export * from './create-auth.repository';
 export * from './create-user.repository';
 export * from './delete-user-by-id.repository';
@@ -7,3 +8,4 @@ export * from './find-user-by-email.repository';
 export * from './find-user-by-id.repository';
 export * from './list-user.repository';
 export * from './verify-user-permissions.repository';
+export * from './verify-user-status-by-id.repository';
