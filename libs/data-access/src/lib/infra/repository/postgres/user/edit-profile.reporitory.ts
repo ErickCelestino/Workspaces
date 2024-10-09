8668fe4c12affda7e350edbc92054a267a8ae0dd
Erick Celestino <erickcelestimo@gmail.com>
2024-10-08 23:26:24 -0300

    feat: add edit profile implementation

---
+++ libs/data-access/src/lib/infra/repository/postgres/user/edit-profile.reporitory.ts
@@ -0,0 +1,27 @@
+import { Inject } from '@nestjs/common';
+import { EditProfileDto, EditProfileRepository } from '@workspaces/domain';
+import { PrismaService } from 'nestjs-prisma';
+
+export class EditProfileRepositoryImpl implements EditProfileRepository {
+  constructor(@Inject('PrismaService') private prismaService: PrismaService) {}
+  async edit(input: EditProfileDto): Promise<string> {
+    const {
+      body: { id, name, birthDate },
+    } = input;
+
+    const editedProfile = await this.prismaService.user.update({
+      where: {
+        user_id: id,
+      },
+      data: {
+        name,
+        ...(Object.keys({ birthDate }).length > 1
+          ? { birth_date: birthDate }
+          : {}),
+        updated_at: new Date(),
+      },
+    });
+
+    return editedProfile?.user_id ?? '';
+  }
+}
--- libs/data-access/src/lib/infra/repository/postgres/user/index.ts
+++ libs/data-access/src/lib/infra/repository/postgres/user/index.ts
@@ -3,6 +3,7 @@ export * from './change-user-type.repository';
 export * from './create-auth.repository';
 export * from './create-user.repository';
 export * from './delete-user-by-id.repository';
+export * from './edit-profile.reporitory';
 export * from './edit-user.repository';
 export * from './filter-by-email-or-nickname.repository';
 export * from './find-user-by-email.repository';
