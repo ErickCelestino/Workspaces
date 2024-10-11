98d783510aca6226a51c8bbe1ccf839cbdac4702
ErickEmpresa <erickcelestimo@gmail.com>
2024-10-11 16:04:54 -0300

    feat: add button navigation in user progile container component

--- libs/feature/src/lib/containers/user/user-profile.container.tsx
+++ libs/feature/src/lib/containers/user/user-profile.container.tsx
@@ -1,27 +1,143 @@
-import { Box } from '@mui/material';
+import {
+  BottomNavigation,
+  BottomNavigationAction,
+  Box,
+  useTheme,
+} from '@mui/material';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import StoreIcon from '@mui/icons-material/Store';
+import { FC, useCallback, useEffect, useRef, useState } from 'react';
 import { ToolbarPureTV, UserProfileImage } from '../../components';
 import { LayoutBase } from '../../layout';
+import { useLoggedUser } from '../../contexts';
+import { useFindUserByIdData, useSnackbarAlert } from '../../hooks';
 
-export const UserProfileContainer = () => {
+interface UserProfileContainerProps {
+  userLabel?: string;
+  companyLabel?: string;
+}
+
+export const UserProfileContainer: FC<UserProfileContainerProps> = ({
+  userLabel = 'Perfil',
+  companyLabel = 'Empresas',
+}) => {
+  const { loggedUser } = useLoggedUser();
+  const hasLoadedUserData = useRef(false);
+  const theme = useTheme();
+  const { showSnackbarAlert, SnackbarAlert } = useSnackbarAlert();
+
+  const [selectedTab, setSelectedTab] = useState(0);
+
+  const showAlert = useCallback(
+    (message: string, success: boolean) => {
+      showSnackbarAlert({
+        message: message,
+        severity: success ? 'success' : 'error',
+      });
+    },
+    [showSnackbarAlert]
+  );
+
+  const { getUserByIdData, userById } = useFindUserByIdData({
+    showAlert,
+    loggedUserId: loggedUser?.id ?? '',
+  });
+
   const editProfile = () => {
     //aa
   };
+
+  useEffect(() => {
+    if (!hasLoadedUserData.current) {
+      getUserByIdData();
+      hasLoadedUserData.current = true;
+    }
+  }, [getUserByIdData]);
+
+  const renderContent = () => {
+    switch (selectedTab) {
+      case 0:
+        return <div>Home Content</div>;
+      case 1:
+        return <div>Search Content</div>;
+      case 2:
+        return <div>Profile Content</div>;
+      default:
+        return null;
+    }
+  };
+
   return (
-    <LayoutBase title="Perfil do Usuário" toolBar={<ToolbarPureTV />}>
-      <Box
-        sx={{
-          display: 'flex',
-          justifyContent: 'center',
-        }}
-      >
-        <UserProfileImage
-          editProfile={editProfile}
-          image=""
-          height={60}
-          imageHeight={20}
-          imageWidth={20}
-        />
-      </Box>
-    </LayoutBase>
+    <>
+      <LayoutBase title="Perfil do Usuário" toolBar={<ToolbarPureTV />}>
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            flexDirection: 'column',
+          }}
+        >
+          <UserProfileImage
+            editProfile={editProfile}
+            image={userById.userImage}
+            height={30}
+            imageHeight={20}
+            imageWidth={20}
+          />
+
+          <Box sx={{ pb: 7 }}>
+            <Box
+              sx={{
+                display: 'flex',
+                width: '100%',
+                justifyContent: 'center',
+              }}
+            >
+              <BottomNavigation
+                value={selectedTab}
+                sx={{
+                  width: theme.spacing(80),
+                }}
+                onChange={(event, newValue) => setSelectedTab(newValue)}
+              >
+                <BottomNavigationAction
+                  label={userLabel}
+                  icon={
+                    <AccountCircleIcon
+                      sx={{
+                        width: theme.spacing(4),
+                        height: theme.spacing(3.5),
+                      }}
+                    />
+                  }
+                />
+                <BottomNavigationAction
+                  label={companyLabel}
+                  icon={
+                    <StoreIcon
+                      sx={{
+                        width: theme.spacing(4),
+                        height: theme.spacing(3.5),
+                      }}
+                    />
+                  }
+                />
+              </BottomNavigation>
+            </Box>
+            <Box
+              sx={{
+                p: 3,
+                minHeight: 'calc(100vh - 56px)',
+                display: 'flex',
+                justifyContent: 'center',
+              }}
+            >
+              {renderContent()}
+            </Box>
+          </Box>
+        </Box>
+      </LayoutBase>
+      {SnackbarAlert}
+    </>
   );
 };
