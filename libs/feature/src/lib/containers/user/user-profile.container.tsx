72840d478eb17941853479219b7944c11ce62b7b
ErickEmpresa <erickcelestimo@gmail.com>
2024-10-11 11:41:17 -0300

    feat: add initial user profile container component

--- libs/feature/src/lib/containers/user/user-profile.container.tsx
+++ libs/feature/src/lib/containers/user/user-profile.container.tsx
@@ -1,11 +1,27 @@
 import { Box } from '@mui/material';
-import { ToolbarPureTV } from '../../components';
+import { ToolbarPureTV, UserProfileImage } from '../../components';
 import { LayoutBase } from '../../layout';
 
 export const UserProfileContainer = () => {
+  const editProfile = () => {
+    //aa
+  };
   return (
     <LayoutBase title="Perfil do Usuário" toolBar={<ToolbarPureTV />}>
-      <Box>aa</Box>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+        }}
+      >
+        <UserProfileImage
+          editProfile={editProfile}
+          image=""
+          height={60}
+          imageHeight={20}
+          imageWidth={20}
+        />
+      </Box>
     </LayoutBase>
   );
 };
