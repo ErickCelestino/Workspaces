1ab9ccc0e9d005f45cad5df7aa3453028f06fed3
Erick Celestino <erickcelestimo@gmail.com>
2024-11-12 14:07:12 -0300

    refactor: ajust mobile ui component

--- libs/feature/src/lib/containers/marketing/pre-registration/pre-registration.container.tsx
+++ libs/feature/src/lib/containers/marketing/pre-registration/pre-registration.container.tsx
@@ -1,7 +1,13 @@
-import { Box, Card, CardContent, Typography } from '@mui/material';
+import {
+  Box,
+  Card,
+  CardContent,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from '@mui/material';
 import { FC, useEffect } from 'react';
 import { FormPreRegistration } from '../../../components';
-//import { useNavigate } from "react-router-dom";
 
 interface PreRegistrationContainerProps {
   title: string;
@@ -10,7 +16,8 @@ interface PreRegistrationContainerProps {
 export const PreRegistrationContainer: FC<PreRegistrationContainerProps> = ({
   title,
 }) => {
-  //const navigate = useNavigate();
+  const theme = useTheme();
+  const smDown = useMediaQuery(theme.breakpoints.down('sm'));
 
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
@@ -25,8 +32,45 @@ export const PreRegistrationContainer: FC<PreRegistrationContainerProps> = ({
       alignItems="center"
       minHeight="100vh"
     >
-      <Card sx={{ maxWidth: 400, padding: 2, boxShadow: 3 }}>
-        <CardContent>
+      {!smDown && (
+        <Card sx={{ maxWidth: 400, padding: 2, boxShadow: 3 }}>
+          <CardContent>
+            <Typography
+              fontWeight={800}
+              variant="h5"
+              component="div"
+              gutterBottom
+            >
+              {title}
+            </Typography>
+
+            <FormPreRegistration
+              nameLabel="Nome"
+              companyNameLabel="Nome da Empresa"
+              ladingpageUse={{
+                ladingpageUseLabel: 'Para que você quer usar sua landing page?',
+                ladingpageUseList: [
+                  'Gerar vendas',
+                  'Capturar contatos (leads)',
+                  'Divulgar algo (evento, marca, etc.)',
+                ],
+              }}
+              ladingPageemphasis={{
+                ladingPageemphasisLabel:
+                  'Você já sabe o que gostaria de destacar na sua landing page?',
+                ladingPageemphasisList: [
+                  'Sim, já tenho uma ideia clara.',
+                  'Tenho algumas ideias, mas preciso de orientação.',
+                  'Não, preciso de ajuda desde o início.',
+                ],
+              }}
+            />
+          </CardContent>
+        </Card>
+      )}
+
+      {smDown && (
+        <Box sx={{ display: 'flex', flexDirection: 'column', maxWidth: '80%' }}>
           <Typography
             fontWeight={800}
             variant="h5"
@@ -57,8 +101,8 @@ export const PreRegistrationContainer: FC<PreRegistrationContainerProps> = ({
               ],
             }}
           />
-        </CardContent>
-      </Card>
+        </Box>
+      )}
     </Box>
   );
 };
