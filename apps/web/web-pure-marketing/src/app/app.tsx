cab4d659cdda4d885cad99dae69fcd85ad2d9f89
Erick Celestino <erickcelestimo@gmail.com>
2024-11-12 14:47:23 -0300

    refactor: add select light and dark theme in app theme provider

--- apps/web/web-pure-marketing/src/app/app.tsx
+++ apps/web/web-pure-marketing/src/app/app.tsx
@@ -1,8 +1,13 @@
+import { AppThemeProvider } from '@workspaces/feature';
 import './app.scss';
 import { AppRouters } from './routes/app-routers';
 
 export function App() {
-  return <AppRouters />;
+  return (
+    <AppThemeProvider>
+      <AppRouters />
+    </AppThemeProvider>
+  );
 }
 
 export default App;
