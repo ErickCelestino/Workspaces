1a7878cde0ebea9c97f9397766418855a7f8fd22
Erick Celestino <erickcelestimo@gmail.com>
2024-12-02 22:05:08 -0300

    refactor: ajust prisma service from agnotic database

--- libs/data-access/src/lib/application/services/prisma.service.ts
+++ libs/data-access/src/lib/application/services/prisma.service.ts
@@ -1,26 +1,40 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient as GeneralPrismaClient } from '@workspaces/prisma/general';
 import { PrismaClient as MarketingPrismaClient } from '@workspaces/prisma/marketing';
 
 @Injectable()
-export class PrismaService implements OnModuleInit {
-  generalPrisma: GeneralPrismaClient;
-  marketingPrisma: MarketingPrismaClient;
+export class PrismaService implements OnModuleDestroy {
+  private _generalPrisma: GeneralPrismaClient | null = null;
+  private _marketingPrisma: MarketingPrismaClient | null = null;
 
-  constructor() {
-    this.generalPrisma = new GeneralPrismaClient();
-    this.marketingPrisma = new MarketingPrismaClient();
+  get generalPrisma(): GeneralPrismaClient {
+    if (!this._generalPrisma) {
+      this._generalPrisma = new GeneralPrismaClient();
+      this._generalPrisma
+        .$connect()
+        .then(() => console.log('General DB connected'));
+    }
+    return this._generalPrisma;
   }
 
-  async onModuleInit() {
-    await this.generalPrisma.$connect();
-    await this.marketingPrisma.$connect();
-    console.log('open alls banks');
+  get marketingPrisma(): MarketingPrismaClient {
+    if (!this._marketingPrisma) {
+      this._marketingPrisma = new MarketingPrismaClient();
+      this._marketingPrisma
+        .$connect()
+        .then(() => console.log('Marketing DB connected'));
+    }
+    return this._marketingPrisma;
   }
 
   async onModuleDestroy() {
-    await this.generalPrisma.$disconnect();
-    await this.marketingPrisma.$disconnect();
-    console.log('closed alls banks');
+    if (this._generalPrisma) {
+      await this._generalPrisma.$disconnect();
+      console.log('General DB disconnected');
+    }
+    if (this._marketingPrisma) {
+      await this._marketingPrisma.$disconnect();
+      console.log('Marketing DB disconnected');
+    }
   }
 }
