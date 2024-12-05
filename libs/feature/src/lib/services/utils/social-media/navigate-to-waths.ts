f8b0a656fa2d17b8458f1897aa544515c5984bbc
Erick Celestino <erickcelestimo@gmail.com>
2024-12-05 11:50:58 -0300

    refactor: add props from pre registration container and ajust navigate to waths

--- libs/feature/src/lib/services/utils/social-media/navigate-to-waths.ts
+++ libs/feature/src/lib/services/utils/social-media/navigate-to-waths.ts
@@ -1,3 +1,10 @@
-export const navigateToWaths = (phone: string, message: string) => {
-  window.open(`https://wa.me/${phone}?text=${message}`, '_blank');
+export const navigateToWaths = (
+  phone: string,
+  message: string,
+  inPage?: boolean
+) => {
+  const encodedMessage = encodeURIComponent(message);
+  const url = `https://wa.me/${phone}?text=${encodedMessage}`;
+
+  window.open(url, inPage === true ? '_self' : '_blank');
 };
