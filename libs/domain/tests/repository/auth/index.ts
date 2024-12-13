7d0436aa2597ccfc0ab6db9bdbbb83e2021e6d80
Erick Celestino <erickcelestimo@gmail.com>
2024-12-13 09:41:48 -0300

    test: ensure validations in validate token use case

--- libs/domain/tests/repository/auth/index.ts
+++ libs/domain/tests/repository/auth/index.ts
@@ -3,3 +3,4 @@ export * from './filter-email-or-nickname-empty.mock';
 export * from './filter-email-or-nickname.mock';
 export * from './sign-in.mock';
 export * from './validate-hash.mock';
+export * from './validate-token-expiration.mock';
