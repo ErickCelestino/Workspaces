6af4e074a5b322c3d6077c8244b4e12ff66a198e
Erick Celestino <erickcelestimo@gmail.com>
2024-12-23 23:46:52 -0300

    feat: add create payment model use case

---
+++ libs/domain/src/lib/dto/response/finance/payment-model/payment-model.response.dto.ts
@@ -0,0 +1,10 @@
+export interface PaymentModelResponseDto {
+  id: string;
+  name: string;
+  description: string;
+  status: string;
+  createdAt: Date;
+  createdBy: string;
+  updatedAt: Date;
+  updatedBy: string;
+}
